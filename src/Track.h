b620e18ac530ccd52ce6023a16e031237dc3b491
Paul Licameli <paul.licameli@audacityteam.org>
2018-12-06 16:27:47 -0500

    define TrackViewGroupData

--- src/Track.h
+++ src/Track.h
@@ -192,8 +192,11 @@ template < typename TrackType > struct TrackIterRange;
 
 struct TrackGroupData
    : std::enable_shared_from_this< TrackGroupData >
+   , ClientData::Extensions< TrackGroupData, true >
 {
-   TrackGroupData( const Track &track );
+   using Extensions = ClientData::Extensions< TrackGroupData, true >;
+
+   TrackGroupData( const Track &representative );
    virtual ~TrackGroupData();
    virtual std::shared_ptr< TrackGroupData > Clone() const;
 
@@ -241,7 +244,6 @@ class AUDACITY_DLL_API Track /* not final */
    TrackId mId;
 
  public:
-
    using GroupData = TrackGroupData;
 
  protected:
