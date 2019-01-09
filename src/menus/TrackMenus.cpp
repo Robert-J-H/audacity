6879151d38c71bf6be24eb91ea19dcd873c8bc75
Paul Licameli <paul.licameli@audacityteam.org>
2019-01-09 14:14:40 -0500

    Delay evaluation of checkmark states...
    ... so that more menu item descriptions can be statically constructed once only

--- src/menus/TrackMenus.cpp
+++ src/menus/TrackMenus.cpp
@@ -1581,8 +1581,7 @@ MenuTable::BaseItemPtr TracksMenu( AudacityProject & )
             XXO("&Move Selection with Tracks (on/off)"),
             FN(OnMoveSelectionWithTracks),
             AlwaysEnabledFlag,
-            Options{}.CheckState(
-               gPrefs->Read(wxT("/GUI/MoveSelectionWithTracks"), 0L ) ) )
+            Options{}.CheckTest( wxT("/GUI/MoveSelectionWithTracks"), false ) )
       ),
 
 #if 0
@@ -1622,7 +1621,7 @@ MenuTable::BaseItemPtr TracksMenu( AudacityProject & )
 
       Command( wxT("SyncLock"), XXO("Sync-&Lock Tracks (on/off)"),
          FN(OnSyncLock), AlwaysEnabledFlag,
-         Options{}.CheckState( gPrefs->Read(wxT("/GUI/SyncLockTracks"), 0L) ) )
+         Options{}.CheckTest( wxT("/GUI/SyncLockTracks"), false ) )
 
 #endif
    ) );
