c8e1654b15e18193ba21434c0504211163575f29
Paul Licameli <paul.licameli@audacityteam.org>
2019-01-21 17:45:03 -0500

    static UndoManager::Get()...
    ... not member functions of AudacityProject

--- src/Project.h
+++ src/Project.h
@@ -193,8 +193,6 @@ class AUDACITY_DLL_API AudacityProject final : public wxFrame,
 
    virtual void ApplyUpdatedTheme();
 
-   UndoManager *GetUndoManager() { return mUndoManager.get(); }
-
    sampleFormat GetDefaultFormat() { return mDefaultFormat; }
 
    double GetRate() const { return mRate; }
@@ -579,8 +577,6 @@ public:
    static ODLock &AllProjectDeleteMutex();
 
 private:
-   // History/Undo manager
-   std::unique_ptr<UndoManager> mUndoManager;
    bool mDirty{ false };
 
    // Commands
