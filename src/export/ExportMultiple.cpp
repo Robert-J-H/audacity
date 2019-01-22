078a9e876e90bf373e82a5f94e5761954c00f528
Paul Licameli <paul.licameli@audacityteam.org>
2019-01-22 12:06:46 -0500

    static SelectionState::Get()...
    ... not member functions of AudacityProject

--- src/export/ExportMultiple.cpp
+++ src/export/ExportMultiple.cpp
@@ -45,6 +45,7 @@
 #include "../LabelTrack.h"
 #include "../Project.h"
 #include "../Prefs.h"
+#include "../SelectionState.h"
 #include "../ShuttleGui.h"
 #include "../Tags.h"
 #include "../WaveTrack.h"
@@ -104,7 +105,7 @@ END_EVENT_TABLE()
 
 ExportMultiple::ExportMultiple(AudacityProject *project)
 : wxDialogWrapper(project, wxID_ANY, wxString(_("Export Multiple")))
-, mSelectionState{ project->GetSelectionState() }
+, mSelectionState{ SelectionState::Get( *project ) }
 {
    SetName(GetTitle());
 
